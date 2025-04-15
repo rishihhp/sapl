df78db872a314811db8579cabc21ef0ee193b4c3
rishihhp <rishihhp@stanford.edu>
2025-04-14 18:04:55 -0700

    Update index.js

--- book-finder/js/index.js
+++ book-finder/js/index.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   var item, tile, author, publisher, bookLink, bookImg;
   var outputList = document.getElementById("list-output");
   var bookUrl = "https://www.googleapis.com/books/v1/volumes?q=";
-  var apiKey = "key=AIzaSyDtXC7kb6a7xKJdm_Le6_BYoY5biz6s8Lw";
+  var apiKey = "";
   var placeHldr = '<img src="https://via.placeholder.com/150">';
   var searchData;
 
